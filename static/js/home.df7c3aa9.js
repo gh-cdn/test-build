3af4d786d7e84742156852360970c11b53d45cd8
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-04-20 16:00:00 +0800

    try.

--- static/js/home.df7c3aa9.js
+++
@@ -1,2 +0,0 @@
-/*!  The roject developed by chengpeiquan!  */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["home"],{"864c":function(e,n,t){"use strict";t.r(n);var o=t("7a23"),c={class:"slogan"},s=Object(o["d"])({expose:[],setup:function(e){return console.log("test"),function(e,n){return Object(o["l"])(),Object(o["c"])("div",c," Hello World ")}}});t("ea77"),n["default"]=s},a209:function(e,n,t){},ea77:function(e,n,t){"use strict";t("a209")}}]);
