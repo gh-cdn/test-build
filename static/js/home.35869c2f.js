aa4c2e120f124430815685cef00658ea1af2e18c
chengpeiquan <chengpeiquan@chengpeiquan.com>
2021-04-20 15:12:59 +0800

    try.

--- static/js/home.35869c2f.js
+++
@@ -1,2 +0,0 @@
-/*!  The roject developed by chengpeiquan!  */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["home"],{"786a":function(t,c,e){},"864c":function(t,c,e){"use strict";e.r(c);var o=e("7a23");Object(o["o"])("data-v-3b621c36");var n={class:"slogan"};Object(o["m"])();var a=Object(o["d"])({expose:[],setup:function(t){return console.log("test"),function(t,c){return Object(o["l"])(),Object(o["c"])("div",n," Hello World ")}}});e("940d");a.__scopeId="data-v-3b621c36";c["default"]=a},"940d":function(t,c,e){"use strict";e("786a")}}]);
